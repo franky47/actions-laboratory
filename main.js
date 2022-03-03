e35d4e3758a236a17aac7b9d6655423df9fcd1b2
Francois Best <francois@francoisbest.com>
2022-03-03 09:37:08 +0100

    chore: Give it something to do

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log('Hello, world!')
+// @ts-check
+
+const { fastify } = require('fastify')
+
+const server = fastify({
+  logger: true,
+})
+
+server.get('/', (_req, res) => {
+  res.send({
+    release: process.env.RELEASE_TAG,
+  })
+})
+
+server.listen(process.env.PORT, () => {
+  setTimeout(() => server.close(), 10000)
+})
